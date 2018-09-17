b82098590149027633d53813cbb4abc2583069b4
Quique <Quique@192.168.180.1>
2018-09-16 21:29:00 -0300

    a

--- eureka/src/test/java/com/proyectogrado/pi/eureka/EurekaApplicationTests.java
+++ eureka/src/test/java/com/proyectogrado/pi/eureka/EurekaApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EruekaApplicationTests {
+public class EurekaApplicationTests {
 
 	@Test
 	public void contextLoads() {
