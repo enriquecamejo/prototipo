1cad4952c12e75b6b4d96e31ba5f78e338384388
juan22 <juan.bonhomme22@gmail.com>
2019-02-03 18:13:47 -0300

    Correcciones orquestador

--- orquestador/src/main/java/com/proyectogrado/pi/orquestador/OrquestadorController.java
+++ orquestador/src/main/java/com/proyectogrado/pi/orquestador/OrquestadorController.java
@@ -28,38 +28,38 @@ public class OrquestadorController {
 	
 	@RequestMapping("/orquestador/ejecutarSolucion")
     @ResponseBody
-	public String ejecutarSolucion(@RequestBody String contentMessage, @RequestHeader String idSol) throws Exception {
+	public String ejecutarSolucion(@RequestBody String contenidoMensaje, @RequestHeader String idSol) throws Exception {
 		try {
 			StringBuffer itinerarioPrpty = new StringBuffer("solucion.");
 			itinerarioPrpty.append(idSol).append(".itinerario");
 	        String itinerarioSolucion = env.getProperty(itinerarioPrpty.toString());
 	        String[] listaItinerario = itinerarioSolucion.split(",");
-	        int paso = 1;
-	        Message<String> mensaje = MessageBuilder.withPayload(contentMessage).setHeader("idSol", idSol).setHeader("paso", paso).build();
+	        int paso = 0;
+	        Message<String> mensaje;
 	        String resultado;
 	        for (int i = 0; i< listaItinerario.length; i++) {
 	        	String nombreMicroservicio = listaItinerario[i];
 	        	clientOrquestador.setServiceUrl(nombreMicroservicio);
+	        	paso = paso + 1;
+	    		mensaje = MessageBuilder.withPayload(contenidoMensaje).setHeader("idSol", idSol).setHeader("paso", paso).build();
 	        	try {
 	        		resultado = clientOrquestador.ejecutarServicio(mensaje);
 	        	}catch (Exception e) {
 	        		logger.error("ERROR EN ORQUESTADOR: El componente "+nombreMicroservicio+" dió el error: "+e.getMessage());
-	    			return "No se pudo ejecutar la operación. Vuelva a intenerlo mas tarde.";
+	    			return "No se pudo ejecutar la operación. Vuelva a intentarlo más tarde.";
 				}
 	        	String statusResponse = MensajeCanonicoUtils.obtenerHeaderMensajeCanonico("status", resultado);
 	    		logger.info("STATUS MENSAJE="+statusResponse);
-	    		String contenidoMensaje = MensajeCanonicoUtils.obtenerPayloadMensajeCanonico(resultado);
+	    		contenidoMensaje = MensajeCanonicoUtils.obtenerPayloadMensajeCanonico(resultado);
 	    		if (!"200".equals(statusResponse)) {
 	    			logger.error("ERROR EN ORQUESTADOR: El componente "+nombreMicroservicio+" respondió con código de error "+statusResponse);
 	    			return contenidoMensaje;
 	    		}
-	    		paso = paso + 1;
-	    		mensaje = MessageBuilder.withPayload(contenidoMensaje).setHeader("idSol", idSol).setHeader("paso", paso).build();
 	        }
-			return mensaje.getPayload();
+			return contenidoMensaje;
 		}catch(Exception ex) {
 			logger.error("ERROR EN ORQUESTADOR: "+ex.getMessage());
-			return "No se pudo ejecutar la operación. Vuelva a intenerlo mas tarde.";
+			return "No se pudo ejecutar la operación. Vuelva a intentarlo más tarde.";
 		}
 	}
 
